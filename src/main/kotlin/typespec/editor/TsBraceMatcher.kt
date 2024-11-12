d59f65c418f641017157616c07305695f7d02f56
r.mordechay <r.mordechay@gridx.de>
2024-11-12 22:49:27 +0100

    Added quotes handler

--- src/main/kotlin/typespec/editor/TsBraceMatcher.kt
+++ src/main/kotlin/typespec/editor/TsBraceMatcher.kt
@@ -3,9 +3,7 @@ package typespec.editor
 import com.intellij.lang.BracePair
 import com.intellij.lang.PairedBraceMatcher
 import com.intellij.psi.PsiFile
-import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
-import com.intellij.psi.tree.TokenSet
 import typespec.TsTypes
 
 class TsBraceMatcher : PairedBraceMatcher {
@@ -14,23 +12,14 @@ class TsBraceMatcher : PairedBraceMatcher {
             BracePair(TsTypes.LBRACE, TsTypes.RBRACE, true),
             BracePair(TsTypes.LPAREN, TsTypes.RPAREN, false),
             BracePair(TsTypes.LBRACK, TsTypes.RBRACK, false),
+            BracePair(TsTypes.LBRACK, TsTypes.RBRACK, false),
         )
     }
 
     override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?): Boolean {
-        return contextType in TokenSet.orSet(
-            TokenSet.create(TsTypes.COMMENT),
-            TokenSet.create(
-                TokenType.WHITE_SPACE,
-                TsTypes.SEMICOLON,
-                TsTypes.COMMA,
-                TsTypes.RPAREN,
-                TsTypes.RBRACK,
-                TsTypes.RBRACE,
-                TsTypes.LBRACE
-            )
-        )
+        return true
     }
+
     override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int {
         return openingBraceOffset
     }
---
+++ src/main/kotlin/typespec/editor/TsTypedHandler.kt
@@ -0,0 +1,43 @@
+package typespec.editor
+
+import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorModificationUtil
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+
+
+class TsTypedHandler : TypedHandlerDelegate() {
+    override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
+        if (isQuoteChar(c)) {
+            handleQuote(c, editor)
+            return Result.STOP
+        }
+        return Result.CONTINUE
+    }
+
+    /**
+     *
+     */
+    private fun handleQuote(quoteChar: Char, editor: Editor) {
+        val offset = editor.caretModel.offset
+        val text = editor.document.text
+        if (offset > 0 && offset < text.length) {
+            val prevChar = text[offset - 1]
+            val nextChar = text[offset]
+            if (prevChar == quoteChar && nextChar == quoteChar) {
+                editor.caretModel.moveToOffset(offset + 1)
+                return
+            }
+        }
+        EditorModificationUtil.insertStringAtCaret(editor, quoteChar.toString())
+        editor.caretModel.moveToOffset(offset)
+    }
+
+    /**
+     *
+     */
+    private fun isQuoteChar(c: Char): Boolean {
+        return c == '"' || c == '\'' || c == '`'
+    }
+}
